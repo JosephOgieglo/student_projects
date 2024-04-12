4aeb1723145190a56358a4eef96ad59263dc4734
JosephOgieglo <ligo55@wp.pl>
2024-04-12 10:17:26 +0000

    studentmachine send

---
+++ python/PolishEnglishExercise/main.py
@@ -0,0 +1,25 @@
+slownik = {'chleb': 'bread', 'telefon': 'telephone', 'samochód': 'car',}
+while 1 > 0:
+
+    print('Dostępne opcje: ')
+    print('słowo')
+    print('koniec')
+
+	podaj = input('Wybierz opcje: ')
+
+if (podaj == słowo)
+
+print(slownik[podaj])
+
+
+
+
+
+
+
+
+
+
+
+
+
